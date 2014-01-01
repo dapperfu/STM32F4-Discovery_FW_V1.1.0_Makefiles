f075f2c0ad4bee58bf309dd222fd1d6487987dd6
Jedediah Frey <frey_jed@cat.com>
2014-01-01 17:14:27 -0500

    Added examples from the FreeRTOS v7.3.0 port by A.C. Verbeck posted to the FreeRTOS Forums.
    http://www.freertos.org/Interactive_Frames/Open_Frames.html?http://interactive.freertos.org/forums/135228-stmicroelectronics

---
+++ Project/FreeRTOS_Examples/DiscoveryExamples/Example015/main.c
@@ -0,0 +1,182 @@
+/*
+    FreeRTOS V6.1.0 - Copyright (C) 2010 Real Time Engineers Ltd.
+
+    This file is part of the FreeRTOS distribution.
+
+    This is example #15 from the book Using the FreeRTOS Real Time Kernel - A Practical Guide
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
+    ***NOTE*** The exception to the GPL is included to allow you to distribute
+    a combined work that includes FreeRTOS without being obliged to provide the
+    source code for proprietary components outside of the FreeRTOS kernel.
+    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details. You should have received a copy of the GNU General Public
+    License and the FreeRTOS license exception along with FreeRTOS; if not it
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained
+    by writing to Richard Barry, contact details for whom are available on the
+    FreeRTOS WEB site.
+
+    1 tab == 4 spaces!
+
+    http://www.FreeRTOS.org - Documentation, latest information, license and
+    contact details.
+
+    http://www.SafeRTOS.com - A version that is certified for use in safety
+    critical systems.
+
+    http://www.OpenRTOS.com - Commercial support, development, porting,
+    licensing and training services.
+*/
+
+/* System includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <cross_studio_io.h>
+
+/* CMSIS / hardware includes */
+#include "system_stm32f4xx.h"
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+/* Dimensions the buffer into which messages destined for stdout are placed. */
+#define mainMAX_MSG_LEN	( 80 )
+
+/* The task to be created.  Two instances of this task are created. */
+static void prvPrintTask( void *pvParameters );
+
+/* The function that uses a mutex to control access to standard out. */
+static void prvNewPrintString( const portCHAR *pcString );
+
+/*-----------------------------------------------------------*/
+
+/* Declare a variable of type xSemaphoreHandle.  This is used to reference the
+mutex type semaphore that is used to ensure mutual exclusive access to stdout. */
+xSemaphoreHandle xMutex;
+
+
+int main( void )
+{
+	/* System Initialization. */
+	SystemInit();
+	SystemCoreClockUpdate();
+
+	debug_printf("Example: 15\n");
+	debug_printf("System Core Clock is running at: %dMHz\n",SystemCoreClock/1000000);
+
+    /* Before a semaphore is used it must be explicitly created.  In this example
+	a mutex type semaphore is created. */
+    xMutex = xSemaphoreCreateMutex();
+
+	/* The tasks are going to use a pseudo random delay, seed the random number
+	generator. */
+	srand( 567 );
+
+	/* Only create the tasks if the semaphore was created successfully. */
+	if( xMutex != NULL )
+	{
+		/* Create two instances of the tasks that attempt to write stdout.  The
+		string they attempt to write is passed in as the task parameter.  The tasks
+		are created at different priorities so some pre-emption will occur. */
+		xTaskCreate( prvPrintTask, "Print1", 240, "Task 1 ******************************************\n", 1, NULL );
+		xTaskCreate( prvPrintTask, "Print2", 240, "Task 2 ------------------------------------------\n", 2, NULL );
+
+		/* Start the scheduler so the created tasks start executing. */
+		vTaskStartScheduler();
+	}
+
+    /* If all is well we will never reach here as the scheduler will now be
+    running the tasks.  If we do reach here then it is likely that there was
+    insufficient heap memory available for a resource to be created. */
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+static void prvNewPrintString( const portCHAR *pcString )
+{
+static char cBuffer[ mainMAX_MSG_LEN ];
+
+	/* The semaphore is created before the scheduler is started so already
+	exists by the time this task executes.
+
+	Attempt to take the semaphore, blocking indefinitely if the mutex is not
+	available immediately.  The call to xSemaphoreTake() will only return when
+	the semaphore has been successfully obtained so there is no need to check the
+	return value.  If any other delay period was used then the code must check
+	that xSemaphoreTake() returns pdTRUE before accessing the resource (in this
+	case standard out. */
+	xSemaphoreTake( xMutex, portMAX_DELAY );
+	{
+		/* The following line will only execute once the semaphore has been
+		successfully obtained - so standard out can be accessed freely. */
+		sprintf( cBuffer, "%s", pcString );
+		debug_printf( cBuffer );
+	}
+	xSemaphoreGive( xMutex );
+}
+/*-----------------------------------------------------------*/
+
+static void prvPrintTask( void *pvParameters )
+{
+char *pcStringToPrint;
+
+	/* Two instances of this task are created so the string the task will send
+	to prvNewPrintString() is passed in the task parameter.  Cast this to the
+	required type. */
+	pcStringToPrint = ( char * ) pvParameters;
+
+	for( ;; )
+	{
+		/* Print out the string using the newly defined function. */
+		prvNewPrintString( pcStringToPrint );
+
+		/* Wait a pseudo random time.  Note that rand() is not necessarily
+		re-entrant, but in this case it does not really matter as the code does
+		not care what value is returned.  In a more secure application a version
+		of rand() that is known to be re-entrant should be used - or calls to
+		rand() should be protected using a critical section. */
+		vTaskDelay( ( rand() & 0x1FF ) );
+	}
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationMallocFailedHook( void )
+{
+	/* This function will only be called if an API call to create a task, queue
+	or semaphore fails because there is too little heap RAM remaining - and
+	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h. */
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( xTaskHandle *pxTask, signed char *pcTaskName )
+{
+	/* This function will only be called if a task overflows its stack.  Note
+	that stack overflow checking does slow down the context switch
+	implementation and will only be performed if configCHECK_FOR_STACK_OVERFLOW
+	is set to either 1 or 2 in FreeRTOSConfig.h. */
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+	/* This example does not use the idle hook to perform any processing.  The
+	idle hook will only be called if configUSE_IDLE_HOOK is set to 1 in 
+	FreeRTOSConfig.h. */
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationTickHook( void )
+{
+	/* This example does not use the tick hook to perform any processing.   The
+	tick hook will only be called if configUSE_TICK_HOOK is set to 1 in
+	FreeRTOSConfig.h. */
+}
---
+++ Project/FreeRTOS_Examples/DiscoveryExamples/Example017/main.c
@@ -0,0 +1,131 @@
+//
+//	This FreeRTOS example started life as a demo from Finn Bindeballe's FreeRTOS v7.1.1 port.
+//
+//	This is now example #17.
+//
+//	It is now targeted to the following:
+//	Tool/System		Manufacturer				Version
+//	IDE:			Rowley Associates			CrossWorks v2.3
+//	RTOS:			Real Time Engineers, Ltd.	FreeRTOS v7.3.0
+//	Processor:		STMicroelectronics			STM32F407VG
+//	Board:			STMicroelectronics			STM32F4-discovery
+//
+//    1 tab == 4 spaces!
+//
+//	FreeRTOS Information:
+//		http://www.FreeRTOS.org - Documentation, latest information, license and
+//		contact details.
+//
+//		http://www.SafeRTOS.com - A version that is certified for use in safety
+//		critical systems.
+//
+//		http://www.OpenRTOS.com - Commercial support, development, porting,
+//		licensing and training services.
+//
+
+#include <stdio.h>
+#include <stdint.h>
+#include <cross_studio_io.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
+
+#define	TASK_LOOP	for (;;)
+
+static void trace(const char* str)
+{
+	vTaskSuspendAll();
+	debug_printf(str);
+	xTaskResumeAll();
+}
+
+//
+//	Orange LED (Port D, pin 13)
+//
+static void LED_orange(void *pvParameters)
+{
+	trace("Task: LED_orange started\n");
+    GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+
+	TASK_LOOP {
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_13);
+		vTaskDelay(50 / portTICK_RATE_MS);
+	}
+}
+
+//
+//	Green LED (Port D, pin 12)
+//
+static void LED_green(void *pvParameters)
+{
+	trace("Task: LED_green started\n");
+	GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+
+	TASK_LOOP {
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+		vTaskDelay(60 / portTICK_RATE_MS);
+	}
+}
+
+//
+//	Red LED (Port D, pin 13)
+//
+static void LED_red(void *pvParameters)
+{
+	trace("Task: LED_red started\n");
+	GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+
+	TASK_LOOP {
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_14);
+		vTaskDelay(70 / portTICK_RATE_MS);
+	}
+}
+
+//
+//	Blue LED (Port D, pin 13)
+//
+static void LED_blue(void *pvParameters)
+{
+	trace("Task: LED_blue started\n");
+	GPIO_ResetBits(GPIOD, GPIO_Pin_15);
+
+	TASK_LOOP {
+		GPIO_ToggleBits(GPIOD, GPIO_Pin_15);
+		vTaskDelay(80 / portTICK_RATE_MS);
+	}
+}
+
+static void IO_Init(void)
+{
+	GPIO_InitTypeDef  GPIO_InitStructure;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+}
+
+int main(void)
+{
+	/* System Initialization. */
+	SystemInit();
+	SystemCoreClockUpdate();
+
+	debug_printf("Example: 17\n");
+	debug_printf("System Core Clock is running at: %dMHz\n",SystemCoreClock/1000000);
+
+	IO_Init();
+
+	xTaskCreate(LED_orange, (signed char*)"Orange", 128, NULL, tskIDLE_PRIORITY+1, NULL);
+	xTaskCreate(LED_green,  (signed char*)"Green",  128, NULL, tskIDLE_PRIORITY+1, NULL);
+	xTaskCreate(LED_red,    (signed char*)"Red",    128, NULL, tskIDLE_PRIORITY+1, NULL);
+	xTaskCreate(LED_blue,   (signed char*)"Blue",   128, NULL, tskIDLE_PRIORITY+1, NULL);
+	debug_printf("Scheduler started\n");
+	vTaskStartScheduler();
+}
