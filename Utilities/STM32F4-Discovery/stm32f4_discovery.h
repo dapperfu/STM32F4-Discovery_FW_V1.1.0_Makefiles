a8cd7dd845d259048bc2aa7f6de14f17a9f1166b
Jedediah Frey <frey_jed@cat.com>
2014-01-03 19:31:30 -0500

    Updated stm32f4_discovery.h with Embest modifications from the STM32F4DIS-BB Software Examples.

--- Utilities/STM32F4-Discovery/stm32f4_discovery.h
+++ Utilities/STM32F4-Discovery/stm32f4_discovery.h
@@ -16,10 +16,31 @@
   * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
   * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
   *
-  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  * <h2><center>&copy; Portions COPYRIGHT 2011 STMicroelectronics</center></h2>
   ******************************************************************************  
   */ 
-  
+/**
+  ******************************************************************************
+  * <h2><center>&copy; Portions COPYRIGHT 2012 Embest Tech. Co., Ltd.</center></h2>
+  * @file    stm32f4_discovery.h
+  * @author  CMP Team
+  * @version V1.0.0
+  * @date    28-December-2012
+  * @brief   This file contains definitions for STM32F4-Discovery Kit's Leds and 
+  *          push-button hardware resources.
+  *          Modified to support the STM32F4DISCOVERY, STM32F4DIS-BB, STM32F4DIS-CAM
+  *          and STM32F4DIS-LCD modules.       
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, Embest SHALL NOT BE HELD LIABLE FOR ANY DIRECT, INDIRECT
+  * OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE CONTENT
+  * OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING INFORMATION
+  * CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  ******************************************************************************
+  */    
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32F4_DISCOVERY_H
 #define __STM32F4_DISCOVERY_H
@@ -63,7 +84,18 @@ typedef enum
 {  
   BUTTON_MODE_GPIO = 0,
   BUTTON_MODE_EXTI = 1
-} ButtonMode_TypeDef;     
+} ButtonMode_TypeDef; 
+
+typedef enum 
+{
+  COM1 = 0,
+  COM2 = 1,
+  COM3 = 2,
+  COM4 = 3,
+  COM5 = 4,
+  COM6 = 5,
+} COM_TypeDef; 
+    
 /**
   * @}
   */ 
@@ -114,7 +146,115 @@ typedef enum
 /**
   * @}
   */ 
-  
+
+/** @addtogroup STM32F4_DISCOVERY_LOW_LEVEL_USART
+  * @{
+  */
+#define COMn                             2
+/**
+ * @brief Definition for COM port3, connected to USART1
+ */ 
+#define EVAL_COM3                        USART1
+#define EVAL_COM3_CLK                    RCC_APB2Periph_USART1
+#define EVAL_COM3_TX_PIN                 GPIO_Pin_9
+#define EVAL_COM3_TX_GPIO_PORT           GPIOA
+#define EVAL_COM3_TX_GPIO_CLK            RCC_AHB1Periph_GPIOA
+#define EVAL_COM3_TX_SOURCE              GPIO_PinSource9
+#define EVAL_COM3_TX_AF                  GPIO_AF_USART1
+#define EVAL_COM3_RX_PIN                 GPIO_Pin_10
+#define EVAL_COM3_RX_GPIO_PORT           GPIOA
+#define EVAL_COM3_RX_GPIO_CLK            RCC_AHB1Periph_GPIOA
+#define EVAL_COM3_RX_SOURCE              GPIO_PinSource10
+#define EVAL_COM3_RX_AF                  GPIO_AF_USART1
+#define EVAL_COM3_IRQn                   USART1_IRQn
+
+/**
+ * @brief Definition for COM port2, connected to USART3
+ */
+#define EVAL_COM2                        USART3
+#define EVAL_COM2_CLK                    RCC_APB1Periph_USART3
+#define EVAL_COM2_TX_PIN                 GPIO_Pin_10
+#define EVAL_COM2_TX_GPIO_PORT           GPIOC
+#define EVAL_COM2_TX_GPIO_CLK            RCC_AHB1Periph_GPIOC
+#define EVAL_COM2_TX_SOURCE              GPIO_PinSource10
+#define EVAL_COM2_TX_AF                  GPIO_AF_USART3
+#define EVAL_COM2_RX_PIN                 GPIO_Pin_11
+#define EVAL_COM2_RX_GPIO_PORT           GPIOC
+#define EVAL_COM2_RX_GPIO_CLK            RCC_AHB1Periph_GPIOC
+#define EVAL_COM2_RX_SOURCE              GPIO_PinSource11
+#define EVAL_COM2_RX_AF                  GPIO_AF_USART3
+#define EVAL_COM2_IRQn                   USART3_IRQn
+
+/**
+ * @brief Definition for COM port1, connected to USART4
+ */
+#define EVAL_COM1                        USART6
+#define EVAL_COM1_CLK                    RCC_APB2Periph_USART6
+#define EVAL_COM1_TX_PIN                 GPIO_Pin_6
+#define EVAL_COM1_TX_GPIO_PORT           GPIOC
+#define EVAL_COM1_TX_GPIO_CLK            RCC_AHB1Periph_GPIOC
+#define EVAL_COM1_TX_SOURCE              GPIO_PinSource6
+#define EVAL_COM1_TX_AF                  GPIO_AF_USART6
+#define EVAL_COM1_RX_PIN                 GPIO_Pin_7
+#define EVAL_COM1_RX_GPIO_PORT           GPIOC
+#define EVAL_COM1_RX_GPIO_CLK            RCC_AHB1Periph_GPIOC
+#define EVAL_COM1_RX_SOURCE              GPIO_PinSource7
+#define EVAL_COM1_RX_AF                  GPIO_AF_USART6
+#define EVAL_COM1_IRQn                   USART6_IRQn
+
+/**
+  * @}
+  */ 	  
+
+
+/** @addtogroup STM32F4_DISCOVERY_LOW_LEVEL_SD_FLASH
+  * @{
+  */ 
+/**
+  * @brief  SD FLASH SDIO Interface
+  */
+#define SD_DETECT_PIN                    GPIO_Pin_15                 /* PB.15 */
+#define SD_DETECT_GPIO_PORT              GPIOB                       /* GPIOB */
+#define SD_DETECT_GPIO_CLK               RCC_AHB1Periph_GPIOB
+   
+#define SDIO_FIFO_ADDRESS                ((uint32_t)0x40012C80)
+/** 
+  * @brief  SDIO Intialization Frequency (400KHz max)
+  */
+#define SDIO_INIT_CLK_DIV                ((uint8_t)0x76)
+/** 
+  * @brief  SDIO Data Transfer Frequency (25MHz max) 
+  */
+#define SDIO_TRANSFER_CLK_DIV            ((uint8_t)0x1) 
+
+#define SD_SDIO_DMA                   DMA2
+#define SD_SDIO_DMA_CLK               RCC_AHB1Periph_DMA2
+ 
+#define SD_SDIO_DMA_STREAM3	          3
+//#define SD_SDIO_DMA_STREAM6           6
+
+#ifdef SD_SDIO_DMA_STREAM3
+ #define SD_SDIO_DMA_STREAM            DMA2_Stream3
+ #define SD_SDIO_DMA_CHANNEL           DMA_Channel_4
+ #define SD_SDIO_DMA_FLAG_FEIF         DMA_FLAG_FEIF3
+ #define SD_SDIO_DMA_FLAG_DMEIF        DMA_FLAG_DMEIF3
+ #define SD_SDIO_DMA_FLAG_TEIF         DMA_FLAG_TEIF3
+ #define SD_SDIO_DMA_FLAG_HTIF         DMA_FLAG_HTIF3
+ #define SD_SDIO_DMA_FLAG_TCIF         DMA_FLAG_TCIF3 
+ #define SD_SDIO_DMA_IRQn              DMA2_Stream3_IRQn
+ #define SD_SDIO_DMA_IRQHANDLER        DMA2_Stream3_IRQHandler 
+#elif defined SD_SDIO_DMA_STREAM6
+ #define SD_SDIO_DMA_STREAM            DMA2_Stream6
+ #define SD_SDIO_DMA_CHANNEL           DMA_Channel_4
+ #define SD_SDIO_DMA_FLAG_FEIF         DMA_FLAG_FEIF6
+ #define SD_SDIO_DMA_FLAG_DMEIF        DMA_FLAG_DMEIF6
+ #define SD_SDIO_DMA_FLAG_TEIF         DMA_FLAG_TEIF6
+ #define SD_SDIO_DMA_FLAG_HTIF         DMA_FLAG_HTIF6
+ #define SD_SDIO_DMA_FLAG_TCIF         DMA_FLAG_TCIF6 
+ #define SD_SDIO_DMA_IRQn              DMA2_Stream6_IRQn
+ #define SD_SDIO_DMA_IRQHANDLER        DMA2_Stream6_IRQHandler
+#endif /* SD_SDIO_DMA_STREAM3 */
+
 /** @defgroup STM32F4_DISCOVERY_LOW_LEVEL_Exported_Macros
   * @{
   */  
@@ -130,8 +270,16 @@ void STM_EVAL_LEDInit(Led_TypeDef Led);
 void STM_EVAL_LEDOn(Led_TypeDef Led);
 void STM_EVAL_LEDOff(Led_TypeDef Led);
 void STM_EVAL_LEDToggle(Led_TypeDef Led);
+
 void STM_EVAL_PBInit(Button_TypeDef Button, ButtonMode_TypeDef Button_Mode);
 uint32_t STM_EVAL_PBGetState(Button_TypeDef Button);
+
+void STM_EVAL_COMInit(COM_TypeDef COM, USART_InitTypeDef* USART_InitStruct);
+
+void SD_LowLevel_DeInit(void);
+void SD_LowLevel_Init(void); 
+void SD_LowLevel_DMA_TxConfig(uint32_t *BufferSRC, uint32_t BufferSize);
+void SD_LowLevel_DMA_RxConfig(uint32_t *BufferDST, uint32_t BufferSize);
 /**
   * @}
   */
@@ -155,4 +303,4 @@ uint32_t STM_EVAL_PBGetState(Button_TypeDef Button);
 
  
 
-/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
+/*********** Portions COPYRIGHT 2012 Embest Tech. Co., Ltd.*****END OF FILE****/
