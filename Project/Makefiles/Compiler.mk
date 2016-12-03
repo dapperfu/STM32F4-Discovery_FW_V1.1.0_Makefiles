b94a61af78ef9f93f595428c4895b6d7a50967b2
Jed Frey <jedediahfrey@users.noreply.github.com>
2016-12-03 11:26:21 -0500

    compiler is installed in standard paths.

--- Project/Makefiles/Compiler.mk
+++ Project/Makefiles/Compiler.mk
@@ -4,15 +4,7 @@
 #
 ################################################################################
 ## Compiler programs.
-ifeq ($(TRGT),)
-ifeq ($(OS),Windows_NT)
 TRGT = arm-none-eabi-
-else ifeq ($(shell uname), Linux)
-TRGT = /opt/gcc-arm-none-eabi/bin/arm-none-eabi-
-else
-TRGT = arm-none-eabi-
-endif
-endif
 # Program names.
 CC= $(TRGT)gcc
 CP= $(TRGT)objcopy
